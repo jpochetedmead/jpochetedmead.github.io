c78c02f693be3af228818f13a67bb4457775bc47
Julio Pochet Edmead <jpe3841@gmail.com>
2019-10-13 23:58:41 -0400

    last commits

---
+++ script.js
@@ -0,0 +1,48 @@
+// Browser Message code
+
+alert("Written & Coded By Julio Pochet Edmead");
+
+// Don't Touch Me! code
+document.querySelector('.logo').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+
+
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute ('src','images/firefox2.png');
+  } else {
+    myImage.setAttribute ('src','images/firefox-icon.png');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('.changename');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Welcome to my website ' + myName + ' !';
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = storedName + ' Welcome Back!';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
