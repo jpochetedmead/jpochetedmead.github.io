7ea6066658c65db42f4479f2daf1d470c2339ffc
Julio Pochet Edmead <jpe3841@gmail.com>
2019-10-17 15:26:55 -0400

    Add new emojis, Move images to a more specific folder, modified the javascript

--- script.js
+++ script.js
@@ -2,22 +2,19 @@
 
 alert("Written & Coded By Julio Pochet Edmead");
 
-// Don't Touch Me! code
-document.querySelector('.logo').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
-
-
 // Image switcher code
 
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
-  } else {
-    myImage.setAttribute ('src','images/firefox-icon.png');
+  if(mySrc === 'images/emojis/hi-emoji.gif') {
+    myImage.setAttribute ('src','images/emojis/stop-emoji.gif');
+    alert('Ouch! Stop poking me!');
+  }
+  else {
+    myImage.setAttribute ('src','images/emojis/hi-emoji.gif');
+    alert('Come on...STOP!');
   }
 }
 
