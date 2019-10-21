da587a71c3656c94c80fa158db9c7127d94d892b
Julio Pochet Edmead <jpe3841@gmail.com>
2019-10-21 14:26:16 -0400

    Add Scroll to Top Button, make styles of that button match the website colors, button appears on the bottom right.

--- script.js
+++ script.js
@@ -43,3 +43,23 @@ if(!localStorage.getItem('name')) {
 myButton.onclick = function() {
   setUserName();
 }
+
+//Get the button:
+mybutton = document.getElementById("scroll-top-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 33 || document.documentElement.scrollTop > 33) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
